ffae113e8dbf1139a889750a8f56a6e6db401fb6
Fernando Borretti <eudoxiahp@gmail.com>
2018-10-07 22:26:47 -0300

    Austral checked_sub

--- src/prelude.h
+++ src/prelude.h
@@ -13,6 +13,13 @@ std::tuple<T, bool> austral_checked_add(T lhs, T rhs) {
   return std::make_tuple(result, overflowed);
 }
 
+template<typename T>
+std::tuple<T, bool> austral_checked_sub(T lhs, T rhs) {
+  T result = 0;
+  bool overflowed = __builtin_sub_overflow(lhs, rhs, &result);
+  return std::make_tuple(result, overflowed);
+}
+
 #else
 #error "Checked arithmetic intrinstics not found"
 #endif
