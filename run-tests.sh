983c60f31e16661c41c25851ae12df09fbfa2ef4
Fernando Borretti <eudoxiahp@gmail.com>
2018-10-10 21:23:12 -0300

    compile fib.au

--- run-tests.sh
+++ run-tests.sh
@@ -7,3 +7,6 @@ make test
 echo "test.au"
 ./boreal test/valid/test.au --output=test/valid/test.cpp
 clang++ -shared -fPIC -std=c++11 test/valid/test.cpp
+
+echo "fib.au"
+./boreal test/valid/fib.au --output=test/valid/test.cpp
