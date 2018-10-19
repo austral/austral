e28e99bb73b5de97b20bd5a1331f3dc15196c18b
Fernando Borretti <eudoxiahp@gmail.com>
2018-10-19 20:34:37 -0300

    Test execution

--- run-tests.sh
+++ run-tests.sh
@@ -6,8 +6,9 @@ make boreal
 make test
 
 echo "test.au"
-./boreal test/valid/test.au --output=test/valid/test.cpp
-clang++ -shared -fPIC -std=c++11 test/valid/test.cpp
+./boreal test/valid/test.au --output=test/valid/test.cpp --entrypoint=austral-user:test
+clang++ -std=c++11 test/valid/test.cpp -o test/valid/test.bin
+./test/valid/test.bin
 
 echo "fib.au"
 ./boreal test/valid/fib.au --output=test/valid/fib.cpp
