d339e185de6d32a42bbb1c11fd2908d37a7e70f7
Fernando Borretti <eudoxiahp@gmail.com>
2018-10-19 20:30:46 -0300

    Fix

--- run-tests.sh
+++ run-tests.sh
@@ -39,5 +39,5 @@ clang++ -shared -fPIC -std=c++11 test/valid/size-of.cpp
 
 echo "cffi.au"
 ./boreal test/valid/cffi.au --output=test/valid/cffi.cpp --entrypoint=cffi-test:main
-clang++ test/valid/cffi.cpp -o test/valid/cffi.bin
+clang++ -std=c++11 test/valid/cffi.cpp -o test/valid/cffi.bin
 ./test/valid/cffi.bin
