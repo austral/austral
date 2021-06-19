3f0f82695a96ddfc64655b973fba1e79109708d7
Fernando Borretti <fernando@borretti.me>
2021-06-19 01:01:48 +0000

    Add ModuleNameMap

--- lib/IdentifierMap.ml
+++ lib/IdentifierMap.ml
@@ -6,3 +6,12 @@ module IdentifierMap =
         let compare a b = compare (ident_string a) (ident_string b)
       end
     )
+
+module ModuleNameMap =
+  Map.Make(
+      struct
+        open Identifier
+        type t = module_name
+        let compare a b = compare (mod_name_string a) (mod_name_string b)
+      end
+    )
