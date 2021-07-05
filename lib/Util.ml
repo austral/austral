e3d3a900953f5f0fc2d5d0c066a88fe052cdb58d
Fernando Borretti <fernando@borretti.me>
2021-07-05 20:39:34 +0000

    Implement read_file_to_string

--- lib/Util.ml
+++ lib/Util.ml
@@ -1,5 +1,18 @@
+open Error
+
 let string_explode s =
   List.init (String.length s) (String.get s)
 
 let string_implode l =
   String.init (List.length l) (List.nth l)
+
+let read_file_to_string path =
+  let stream = open_in path in
+  try
+    let len = in_channel_length stream in
+    let str = really_input_string stream len in
+    close_in stream;
+    str
+  with _ ->
+    close_in_noerr stream;
+    err ("Failed to read file: " ^ path)
--- lib/Util.mli
+++ lib/Util.mli
@@ -1,3 +1,5 @@
 val string_explode : string -> char list
 
 val string_implode : char list -> string
+
+val read_file_to_string : string -> string
