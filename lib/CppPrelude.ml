297f1a51baee5150b4546fe9fe579de5aebd49f6
Fernando Borretti <fernando@borretti.me>
2021-07-07 21:09:20 +0000

    Add C++ prelude

---
+++ lib/CppPrelude.ml
@@ -0,0 +1,28 @@
+let prelude = {code|
+#include <stdlib.h>
+
+namespace Austral__Memory {
+    template<typename T>
+    T* Allocate(T value) {
+        T* ptr = (T*) malloc(sizeof(T));
+        return ptr;
+    }
+
+    template<typename T>
+    T Load(T* pointer) {
+        return *pointer;
+    }
+
+    template<typename T>
+    bool Store(T* pointer, T value) {
+        *pointer = value;
+        return false;
+    }
+
+    template<typename T>
+    bool Deallocate(T* pointer) {
+        free(pointer);
+        return false;
+    }
+};
+|code}
---
+++ lib/CppPrelude.mli
@@ -0,0 +1,1 @@
+val prelude : string
