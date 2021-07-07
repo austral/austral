af71082f873781264c3dbb5be756f05fd4729fdc
Fernando Borretti <fernando@borretti.me>
2021-07-07 21:48:39 +0000

    Compile to C++, store code in Compiler value

--- lib/Compiler.ml
+++ lib/Compiler.ml
@@ -3,14 +3,19 @@ open BuiltIn
 open ParserInterface
 open CombiningPass
 open ExtractionPass
+open TypingPass
+open CodeGen
+open CppRenderer
 
-type compiler = Compiler of menv
+type compiler = Compiler of menv * string
 
-let cmenv (Compiler m) = m
+let cmenv (Compiler (m, _)) = m
+
+let ccode (Compiler (_, c)) = c
 
 let empty_compiler =
   let menv = put_module empty_menv memory_module in
-  Compiler menv
+  Compiler (menv, "")
 
 let rec compile_mod c is bs =
   let ci = parse_module_int is
@@ -19,9 +24,10 @@ let rec compile_mod c is bs =
   let combined = combine menv ci cb in
   let semantic = extract menv combined in
   let menv' = put_module (cmenv c) semantic in
-  let _ = menv' in
-  print_endline "Hi!";
-  c
+  let typed = augment_module menv' combined in
+  let cpp = gen_module typed in
+  let code = render_module cpp in
+  Compiler (menv', (ccode c) ^ "\n" ^ code)
 
 let rec compile_multiple c modules =
   match modules with
