4c26ce688c92a12f3e3a4b8cbe581d51bbaec717
Fernando Borretti <fernando@borretti.me>
2021-07-11 20:17:28 +0000

    Implement region map

--- lib/Region.ml
+++ lib/Region.ml
@@ -1,4 +1,6 @@
 open Identifier
+open IdentifierMap
+open Error
 
 type region = Region of identifier * int
 [@@deriving eq, show]
@@ -15,3 +17,17 @@ let fresh_region n =
   let i = !region_counter in
   region_counter := i + 1;
   Region (n, i)
+
+type region_map = region IdentifierMap.t
+
+let empty_region_map = IdentifierMap.empty
+
+let get_region m n =
+  IdentifierMap.find_opt n m
+
+let add_region m n r =
+  match get_region m n with
+  | Some _ ->
+     err "Region map already has a region with this name"
+  | None ->
+     IdentifierMap.add n r m
--- lib/Region.mli
+++ lib/Region.mli
@@ -10,3 +10,11 @@ val static_region_name : identifier
 val static_region : region
 
 val fresh_region : identifier -> region
+
+type region_map
+
+val empty_region_map : region_map
+
+val add_region : region_map -> identifier -> region -> region_map
+
+val get_region : region_map -> identifier -> region option
