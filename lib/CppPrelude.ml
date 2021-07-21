826b7faf06eede2fbd21da786b29c5b9bb79658b
Fernando Borretti <fernando@borretti.me>
2021-07-21 21:20:25 +0000

    Only write the value to the pointer if it's non-NULL

--- lib/CppPrelude.ml
+++ lib/CppPrelude.ml
@@ -36,7 +36,9 @@ namespace A_Austral__Memory {
     template<typename T>
     T* A_Allocate(T value) {
         T* ptr = (T*) malloc(sizeof(T));
-        *ptr = value;
+        if (ptr != NULL) {
+            *ptr = value;
+        }
         return ptr;
     }
 
