4c6bef09ccf6f3d2e6b1f6f17ea309ce59af99dd
Fernando Borretti <fernando@borretti.me>
2021-07-22 21:30:19 +0000

    Check the output of test programs is correct

--- run-tests.sh
+++ run-tests.sh
@@ -5,25 +5,29 @@ function compile() {
     ./austral --module=$1/$2 --entrypoint=Example.$2:Main > code.cpp
     g++ code.cpp -o testbin
     rm code.cpp
-    ./testbin
+    ./testbin > actual.txt
+    echo -n -e "$3" > expected.txt
+    diff actual.txt expected.txt
     rm testbin
+    rm actual.txt
+    rm expected.txt
 }
 
-compile examples/constant Constant
-compile examples/identity Identity
-compile examples/ffi FFI
-compile examples/fib Fibonacci
-compile examples/named-argument NamedArgument
-compile examples/memory Memory
-compile examples/type-alias TypeAlias
-compile examples/record Record
-compile examples/union Union
-compile examples/concrete-typeclass ConcreteTypeclass
-compile examples/generic-record GenericRecord
-compile examples/generic-union GenericUnion
-compile examples/generic-typeclass GenericTypeclass
-compile examples/string String
-compile examples/box Box
-compile examples/pointer-to-record PTR
-compile examples/ref-to-record RTR
-compile examples/array Array
+compile examples/constant Constant ""
+compile examples/identity Identity ""
+compile examples/ffi FFI "aHello, world!\n"
+compile examples/fib Fibonacci ""
+compile examples/named-argument NamedArgument ""
+compile examples/memory Memory ""
+compile examples/type-alias TypeAlias ""
+compile examples/record Record ""
+compile examples/union Union ""
+compile examples/concrete-typeclass ConcreteTypeclass ""
+compile examples/generic-record GenericRecord ""
+compile examples/generic-union GenericUnion ""
+compile examples/generic-typeclass GenericTypeclass ""
+compile examples/string String "HHello, world!\n"
+compile examples/box Box "aaa"
+compile examples/pointer-to-record PTR "a"
+compile examples/ref-to-record RTR "aa"
+compile examples/array Array "TFT"
