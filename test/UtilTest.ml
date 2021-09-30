92164f8e3123d211d82cd40d43c13c67cbabe57f
Fernando Borretti <fernando@borretti.me>
2021-09-30 22:31:24 +0000

    Remove print calls

--- test/UtilTest.ml
+++ test/UtilTest.ml
@@ -4,10 +4,6 @@ open TestUtil
 
 let test_run_command _ =
   let (CommandOutput { command; code; stdout; stderr }) = run_command "echo derp" in
-  print_endline ("Code: " ^ (string_of_int code));
-  print_endline ("Command: " ^ command);
-  print_endline ("stdout: " ^ stdout);
-  print_endline ("stderr: " ^ stderr);
   eq "echo derp" command;
   eq 0 code;
   eq "derp" stdout;
