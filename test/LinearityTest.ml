fe61402eecae0df7cf5267be36ddaf5204f1dae2
Fernando Borretti <fernando@borretti.me>
2021-10-08 19:51:05 +0000

    Add a couple of unit tests

--- test/LinearityTest.ml
+++ test/LinearityTest.ml
@@ -203,6 +203,92 @@ end module body.
     Austral_error _ ->
     assert_bool "Passed" true
 
+(* Test that forgetting to consume a linear value bound in a `case` statement fails. *)
+let test_forget_case_binding _ =
+  let i = {code|
+
+module Example is
+    function Main(root: Root_Capability): Root_Capability;
+end module.
+
+|code}
+  and b = {code|
+
+module body Example is
+    record R : Linear is
+        x: Integer_32;
+    end;
+
+    function Main(root: Root_Capability): Root_Capability is
+        let r: R := R(x => 32);
+        let ropt: Optional[R] := Some(r);
+        case ropt of
+            when Some(value: R) do
+                skip;
+            end;
+            when None do
+                skip;
+            end;
+        end case;
+        return root;
+    end;
+end module body.
+
+|code}
+  in
+  try
+    let _ = compile_and_run [(i, b)] "Example:Main" in
+    assert_failure "This should have failed."
+  with
+    Austral_error _ ->
+    assert_bool "Passed" true
+
+(* Test that you can't consume a linear value twice in a case statement. *)
+let test_consume_case_binding_twice _ =
+  let i = {code|
+
+module Example is
+    function Main(root: Root_Capability): Root_Capability;
+end module.
+
+|code}
+  and b = {code|
+
+module body Example is
+    record R : Linear is
+        x: Integer_32;
+    end;
+
+    function Consume(r: R): Unit is
+        let r: R := R(x => 32);
+        return nil;
+    end;
+
+    function Main(root: Root_Capability): Root_Capability is
+        let r: R := R(x => 32);
+        let ropt: Optional[R] := Some(r);
+        case ropt of
+            when Some(value: R) do
+                Consume(value);
+                Consume(value);
+            end;
+            when None do
+                skip;
+            end;
+        end case;
+        return root;
+    end;
+end module body.
+
+|code}
+  in
+  try
+    let _ = compile_and_run [(i, b)] "Example:Main" in
+    assert_failure "This should have failed."
+  with
+    Austral_error _ ->
+    assert_bool "Passed" true
+
 
 let suite =
   "Linearity checker tests" >::: [
@@ -211,7 +297,9 @@ let suite =
       "Consume record in if statement" >:: test_consume_if;
       "Consume record in one branch of an if statement" >:: test_consume_if_asymmetrically;
       "Consume by unwrapping twice" >:: test_unwrap_twice;
-      "Consume by calling a function twice" >:: test_funcall_twice
+      "Consume by calling a function twice" >:: test_funcall_twice;
+      "Forget a case binding" >:: test_forget_case_binding;
+      "Consume a case binding twice" >:: test_consume_case_binding_twice
     ]
 
 let _ = run_test_tt_main suite
