2cf89bab5f85415c47cd7fddbf6c6cde09312548
Fernando Borretti <fernando@borretti.me>
2021-10-10 02:17:17 +0000

    Fix reporting of lines and printing of code spans in errors

--- lib/Span.ml
+++ lib/Span.ml
@@ -43,7 +43,7 @@ let from_loc (start_pos, end_pos): span =
     }
 
 let position_to_string (Position { line; column }): string =
-  "line " ^ (string_of_int line) ^ ", column " ^ (string_of_int column)
+  "line " ^ (string_of_int (line + 1)) ^ ", column " ^ (string_of_int column)
 
 let span_to_string (Span { filename; startp; endp; }): string =
   "Filename: '" ^ filename ^ "'\nFrom: " ^ (position_to_string startp) ^ "\nTo: " ^ (position_to_string endp)
@@ -62,10 +62,10 @@ let span_text (code: string) (Span { startp; endp; _}): string =
   let context_line_count: int = 2 in
   (* What's the index of the first line to collect? We use max to ensure it's
      non-negative. *)
-  let first_line_idx: int = max (((pos_line startp) - 1) - context_line_count) 0 in
+  let first_line_idx: int = max ((pos_line startp) - context_line_count) 0 in
   (* What's the index of the last line to collect? We use min to ensure it's not
      greater than the furthest line in the file. *)
-  let last_line_idx: int = min (((pos_line endp) - 1) + context_line_count) (List.length lines) in
+  let last_line_idx: int = min ((pos_line endp) + context_line_count) (List.length lines) in
   (* Collect the lines. *)
   let collected_lines: string list =
     List.filteri (fun idx _ -> idx >= first_line_idx && idx <= last_line_idx) lines in
