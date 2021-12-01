02a59a65518ac045c84d4dcebd0075479fd47c29
Fernando Borretti <fernando@borretti.me>
2021-12-01 21:23:51 +0000

    Add index type conversion functions to prelude

--- lib/CppPrelude.ml
+++ lib/CppPrelude.ml
@@ -54,6 +54,7 @@ namespace Austral__Core {
     int16_t convert_uint8_to_int16(uint8_t value) { return (int16_t)(value); }
     int32_t convert_uint8_to_int32(uint8_t value) { return (int32_t)(value); }
     int64_t convert_uint8_to_int64(uint8_t value) { return (int64_t)(value); }
+    size_t convert_uint8_to_usize(uint8_t value) { return (size_t)(value); }
     float convert_uint8_to_f(uint8_t value) { return (float)(value); }
     double convert_uint8_to_d(uint8_t value) { return (double)(value); }
     uint8_t convert_uint16_to_uint8(uint16_t value) { return (uint8_t)(value); }
@@ -64,6 +65,7 @@ namespace Austral__Core {
     int16_t convert_uint16_to_int16(uint16_t value) { return (int16_t)(value); }
     int32_t convert_uint16_to_int32(uint16_t value) { return (int32_t)(value); }
     int64_t convert_uint16_to_int64(uint16_t value) { return (int64_t)(value); }
+    size_t convert_uint16_to_usize(uint16_t value) { return (size_t)(value); }
     float convert_uint16_to_f(uint16_t value) { return (float)(value); }
     double convert_uint16_to_d(uint16_t value) { return (double)(value); }
     uint8_t convert_uint32_to_uint8(uint32_t value) { return (uint8_t)(value); }
@@ -74,6 +76,7 @@ namespace Austral__Core {
     int16_t convert_uint32_to_int16(uint32_t value) { return (int16_t)(value); }
     int32_t convert_uint32_to_int32(uint32_t value) { return (int32_t)(value); }
     int64_t convert_uint32_to_int64(uint32_t value) { return (int64_t)(value); }
+    size_t convert_uint32_to_usize(uint32_t value) { return (size_t)(value); }
     float convert_uint32_to_f(uint32_t value) { return (float)(value); }
     double convert_uint32_to_d(uint32_t value) { return (double)(value); }
     uint8_t convert_uint64_to_uint8(uint64_t value) { return (uint8_t)(value); }
@@ -84,6 +87,7 @@ namespace Austral__Core {
     int16_t convert_uint64_to_int16(uint64_t value) { return (int16_t)(value); }
     int32_t convert_uint64_to_int32(uint64_t value) { return (int32_t)(value); }
     int64_t convert_uint64_to_int64(uint64_t value) { return (int64_t)(value); }
+    size_t convert_uint64_to_usize(uint64_t value) { return (size_t)(value); }
     float convert_uint64_to_f(uint64_t value) { return (float)(value); }
     double convert_uint64_to_d(uint64_t value) { return (double)(value); }
     uint8_t convert_int8_to_uint8(int8_t value) { return (uint8_t)(value); }
@@ -94,6 +98,7 @@ namespace Austral__Core {
     int16_t convert_int8_to_int16(int8_t value) { return (int16_t)(value); }
     int32_t convert_int8_to_int32(int8_t value) { return (int32_t)(value); }
     int64_t convert_int8_to_int64(int8_t value) { return (int64_t)(value); }
+    size_t convert_int8_to_usize(int8_t value) { return (size_t)(value); }
     float convert_int8_to_f(int8_t value) { return (float)(value); }
     double convert_int8_to_d(int8_t value) { return (double)(value); }
     uint8_t convert_int16_to_uint8(int16_t value) { return (uint8_t)(value); }
@@ -104,6 +109,7 @@ namespace Austral__Core {
     int16_t convert_int16_to_int16(int16_t value) { return (int16_t)(value); }
     int32_t convert_int16_to_int32(int16_t value) { return (int32_t)(value); }
     int64_t convert_int16_to_int64(int16_t value) { return (int64_t)(value); }
+    size_t convert_int16_to_usize(int16_t value) { return (size_t)(value); }
     float convert_int16_to_f(int16_t value) { return (float)(value); }
     double convert_int16_to_d(int16_t value) { return (double)(value); }
     uint8_t convert_int32_to_uint8(int32_t value) { return (uint8_t)(value); }
@@ -114,6 +120,7 @@ namespace Austral__Core {
     int16_t convert_int32_to_int16(int32_t value) { return (int16_t)(value); }
     int32_t convert_int32_to_int32(int32_t value) { return (int32_t)(value); }
     int64_t convert_int32_to_int64(int32_t value) { return (int64_t)(value); }
+    size_t convert_int32_to_usize(int32_t value) { return (size_t)(value); }
     float convert_int32_to_f(int32_t value) { return (float)(value); }
     double convert_int32_to_d(int32_t value) { return (double)(value); }
     uint8_t convert_int64_to_uint8(int64_t value) { return (uint8_t)(value); }
@@ -124,8 +131,20 @@ namespace Austral__Core {
     int16_t convert_int64_to_int16(int64_t value) { return (int16_t)(value); }
     int32_t convert_int64_to_int32(int64_t value) { return (int32_t)(value); }
     int64_t convert_int64_to_int64(int64_t value) { return (int64_t)(value); }
+    size_t convert_int64_to_usize(int64_t value) { return (size_t)(value); }
     float convert_int64_to_f(int64_t value) { return (float)(value); }
     double convert_int64_to_d(int64_t value) { return (double)(value); }
+    uint8_t convert_usize_to_uint8(size_t value) { return (uint8_t)(value); }
+    uint16_t convert_usize_to_uint16(size_t value) { return (uint16_t)(value); }
+    uint32_t convert_usize_to_uint32(size_t value) { return (uint32_t)(value); }
+    uint64_t convert_usize_to_uint64(size_t value) { return (uint64_t)(value); }
+    int8_t convert_usize_to_int8(size_t value) { return (int8_t)(value); }
+    int16_t convert_usize_to_int16(size_t value) { return (int16_t)(value); }
+    int32_t convert_usize_to_int32(size_t value) { return (int32_t)(value); }
+    int64_t convert_usize_to_int64(size_t value) { return (int64_t)(value); }
+    size_t convert_usize_to_usize(size_t value) { return (size_t)(value); }
+    float convert_usize_to_f(size_t value) { return (float)(value); }
+    double convert_usize_to_d(size_t value) { return (double)(value); }
     uint8_t convert_f_to_uint8(float value) { return (uint8_t)(value); }
     uint16_t convert_f_to_uint16(float value) { return (uint16_t)(value); }
     uint32_t convert_f_to_uint32(float value) { return (uint32_t)(value); }
@@ -134,6 +153,7 @@ namespace Austral__Core {
     int16_t convert_f_to_int16(float value) { return (int16_t)(value); }
     int32_t convert_f_to_int32(float value) { return (int32_t)(value); }
     int64_t convert_f_to_int64(float value) { return (int64_t)(value); }
+    size_t convert_f_to_usize(float value) { return (size_t)(value); }
     float convert_f_to_f(float value) { return (float)(value); }
     double convert_f_to_d(float value) { return (double)(value); }
     uint8_t convert_d_to_uint8(double value) { return (uint8_t)(value); }
@@ -144,6 +164,7 @@ namespace Austral__Core {
     int16_t convert_d_to_int16(double value) { return (int16_t)(value); }
     int32_t convert_d_to_int32(double value) { return (int32_t)(value); }
     int64_t convert_d_to_int64(double value) { return (int64_t)(value); }
+    size_t convert_d_to_usize(double value) { return (size_t)(value); }
     float convert_d_to_f(double value) { return (float)(value); }
     double convert_d_to_d(double value) { return (double)(value); }
 }
