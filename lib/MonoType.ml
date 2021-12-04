fcd7640c48bfd8f80bfeccf5e1105277ed4160f9
Fernando Borretti <fernando@borretti.me>
2021-12-04 01:21:22 +0000

    Add MonoType module with a type to represent concrete types

---
+++ lib/MonoType.ml
@@ -0,0 +1,15 @@
+open Identifier
+open Type
+
+type mono_type_id = int
+
+type mono_ty =
+  | MonoUnit
+  | MonoBoolean
+  | MonoInteger of signedness * integer_width
+  | MonoSingleFloat
+  | MonoDoubleFloat
+  | NamedType of qident * mono_ty list * mono_type_id
+  | MonoArray of mono_ty
+  | MonoReadRef of mono_ty
+  | MoonWriteRef of mono_ty
---
+++ lib/MonoType.mli
@@ -0,0 +1,19 @@
+(** The monomorphic type system. *)
+open Identifier
+open Type
+
+(** The ID of a type monomorph. *)
+type mono_type_id
+
+(** A monomorphic, or concrete, type. *)
+type mono_ty =
+  | MonoUnit
+  | MonoBoolean
+  | MonoInteger of signedness * integer_width
+  | MonoSingleFloat
+  | MonoDoubleFloat
+  (** A monomorphic instance of a generic type, identified by the name of the type, its monomorphic arguments, and a monomorph ID. *)
+  | NamedType of qident * mono_ty list * mono_type_id
+  | MonoArray of mono_ty
+  | MonoReadRef of mono_ty
+  | MoonWriteRef of mono_ty
