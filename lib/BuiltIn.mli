46c9075385dc68f8069e0c03dac8906d4c53a5bc
Fernando Borretti <fernando@borretti.me>
2021-12-04 02:27:17 +0000

    Hide constant

--- lib/BuiltIn.mli
+++ lib/BuiltIn.mli
@@ -16,8 +16,6 @@ val pervasive_imports: concrete_import_list
 
 val memory_module_name : module_name
 
-val pointer_type_name : identifier
-
 val memory_module : semantic_module
 
 val is_pointer_type : qident -> bool
