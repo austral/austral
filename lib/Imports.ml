23e572efa2a1602fd36915dc1214990903b7dab1
Fernando Borretti <fernando@borretti.me>
2022-02-22 20:19:49 +0000

    Guard against adding symbols twice

--- lib/Imports.ml
+++ lib/Imports.ml
@@ -16,13 +16,21 @@ let empty_map (n: module_name): import_map =
       instances = [];
     }
 
+let get_symbol (im: import_map) (name: identifier): qident option =
+  let (ImportMap { symbols; _ }) = im in
+  IdentifierMap.find_opt name symbols
+
 let add_symbol (im: import_map) (q: qident) =
-  let (ImportMap { import_into; symbols; instances }) = im in
-  ImportMap {
-      import_into = import_into;
-      symbols = IdentifierMap.add (local_name q) q symbols;
-      instances = instances
-    }
+  match get_symbol im (local_name q) with
+  | Some _ ->
+     err "Cannot add symbol to the import map twice."
+  | None ->
+     let (ImportMap { import_into; symbols; instances }) = im in
+     ImportMap {
+         import_into = import_into;
+         symbols = IdentifierMap.add (local_name q) q symbols;
+         instances = instances
+       }
 
 let add_instance (im: import_map) (id: decl_id): import_map =
   let (ImportMap { import_into; symbols; instances }) = im in
@@ -35,10 +43,6 @@ let add_instance (im: import_map) (id: decl_id): import_map =
 let importing_module (im: import_map): module_name =
   let (ImportMap { import_into; _ }) = im in
   import_into
-
-let get_symbol (im: import_map) (name: identifier) =
-  let (ImportMap { symbols; _ }) = im in
-  IdentifierMap.find_opt name symbols
 
 let imported_instances (im: import_map): decl_id list =
   let (ImportMap { instances; _ }) = im in
