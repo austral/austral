7f2bf84bb14ba52f25f357ddc0f7cd60d35ef9fd
Fernando Borretti <fernando@borretti.me>
2022-02-22 20:39:43 +0000

    docstring

--- lib/ExtractionPass.mli
+++ lib/ExtractionPass.mli
@@ -4,6 +4,8 @@ open Combined
 open Semantic
 open ModuleSystem
 
+(** Go through all the types in a combined module, convert them into type
+    signatures, and return those signatures as a list. *)
 val extract_type_signatures : combined_module -> type_signature list
 
 val extract_declarations : module_name -> menv -> type_signature list -> combined_module -> sem_decl list
