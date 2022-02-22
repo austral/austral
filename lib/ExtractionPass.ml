85c691970385d57cdfdbdd965c7d4e3993511b06
Fernando Borretti <fernando@borretti.me>
2022-02-22 21:26:09 +0000

    Some work on updating the extraction pass

--- lib/ExtractionPass.ml
+++ lib/ExtractionPass.ml
@@ -4,14 +4,13 @@ open Type
 open TypeSystem
 open Region
 open Combined
-open Semantic
-open ModuleSystem
+open Env
 open TypeParser
 
-let rec extract_type_signatures (CombinedModule { decls; _ }) =
+let rec extract_type_signatures (CombinedModule { decls; _ }): type_signature list =
   List.filter_map extract_type_signatures' decls
 
-and extract_type_signatures' (def: combined_definition) =
+and extract_type_signatures' (def: combined_definition): type_signature option =
   match def with
   | CConstant _ ->
      None
@@ -28,10 +27,16 @@ and extract_type_signatures' (def: combined_definition) =
   | CInstance _ ->
      None
 
-let rec extract_declarations (module_name: module_name) (menv: menv) local_types (CombinedModule { decls; _ }) =
+let rec extract (env: env) (cmodule: combined_module): env =
+  let (CombinedModule { name; interface_imports; body_imports; _ }) = cmodule in
+  let sigs: type_signature list = extract_type_signatures cmodule in
+  let env: env = extract_declarations env sigs cmodule in
+  env
+
+let rec extract_declarations (env: env) local_types (CombinedModule { name; decls; _ }): decl list =
   List.map (extract_declaration module_name menv local_types) decls
 
-and extract_declaration (module_name: module_name) (menv: menv) (local_types: type_signature list) (def: combined_definition) =
+and extract_declaration (module_name: module_name) (env: env) (local_types: type_signature list) (def: combined_definition): decl list =
   let parse' = parse_type menv local_types in
   let rec parse_slot (typarams: type_parameter list) (QualifiedSlot (n, ts)): typed_slot =
     let rm = region_map_from_typarams typarams in
@@ -67,29 +72,16 @@ and extract_declaration (module_name: module_name) (menv: menv) (local_types: ty
      let rm = region_map_from_typarams typarams in
      STypeClassInstanceDecl (STypeClassInstance (vis, name, typarams, parse' rm typarams argument, List.map (parse_method_def typarams) methods))
 
-let append_if_not_present (list: 'a list) (elem: 'a) (key: 'a -> 'b) =
-  if List.exists (fun el -> (key el) = (key elem)) list then
-    list
-  else
-    List.cons elem list
-
-let rec merge_without_duplicates (a: 'a list) (b: 'a list) (key: 'a -> 'b) =
-  match b with
-  | first::rest -> merge_without_duplicates (append_if_not_present a first key) rest key
-  | [] -> a
-
-let extract menv cmodule =
-  let (CombinedModule { name; interface_imports; body_imports; _ }) = cmodule in
-  let sigs = extract_type_signatures cmodule in
-  let sem_decls = extract_declarations name menv sigs cmodule in
-  (* TODO: This can be safely replaced with the empty list, since it would always be empty. *)
-  let (classes': semantic_typeclass list) = [] in
-  let (instances': semantic_instance list) = merge_without_duplicates (imported_instances interface_imports)
-                   (imported_instances body_imports)
-                   (fun (STypeClassInstance (_, n, _, _, _)) -> n) in
-  SemanticModule {
-      name=name;
-      decls=sem_decls;
-      imported_classes=classes';
-      imported_instances=instances'
-    }
+(** Given the environment, the ID of the module we're extracting, and a combined
+    definition, add all relevant decls to the environment. *)
+let extract_definition (env: env) (mod_id: mod_id) (def: combined_definition): env =
+  match def with
+  | CConstant (vis, name, typespec, _, docstring) ->
+     add_constant env { mod_id; vis; name; docstring }
+  | CTypeAlias (vis, name, typarams, universe, typespec, docstring) ->
+     let rm = region_map_from_typarams typarams in
+     let def = parse' rm typarams typespec in
+     add_type_alias env { mod_id; vis; name; docstring; typarams; universe; def }
+  | CRecord (vis, name, typarams, universe, slots, _) ->
+     let slots = List.map (parse_slot typarams) slots in
+     add_record env { mod_id; vis; name; docstrings; typarams; universe; slots }
