119abf8ace599278da46df2f9babc36fbed30d5c
Fernando Borretti <fernando@borretti.me>
2022-02-22 20:30:25 +0000

    Rewrite import resolution pass to use the new environment

--- lib/ImportResolution.ml
+++ lib/ImportResolution.ml
@@ -6,14 +6,15 @@ open Cst
 open Semantic
 open BuiltIn
 open Error
+open Env
 
 (** Represents an Austral import declaration, that is, in some code like:
 
-        import Foo.Bar (A as D, B, C);
+    {import Foo.Bar (A as D, B, C);}
 
     The first symbol would correspond to this {ImportDecl}:
 
-        ImportDecl ("Foo.Bar", "A", D")
+    {ImportDecl ("Foo.Bar", "A", "D")}
  *)
 type import_decl = ImportDecl of module_name * identifier * identifier
 
@@ -33,12 +34,22 @@ let flatten_imports (list: concrete_import_list list): import_decl list =
              list in
   List.concat ll
 
-let resolve_import (menv: menv) (kind: module_kind) (imports: import_map) (idecl: import_decl): import_map =
+let resolve_import (env: env) (kind: module_kind) (imports: import_map) (idecl: import_decl): import_map =
   let (ImportDecl (module_name, name, nickname)) = idecl in
   let sname = make_sident module_name name in
   match get_decl_by_name env sname with
   | Some decl ->
-     
+     if is_importable decl then
+       match get_symbol imports nickname with
+       | Some _ ->
+          err "Colliding imports"
+       | None ->
+          if (equal_module_name module_name memory_module_name) && (kind = SafeModule) then
+            err "Cannot import from the Austral.Memory module in a safe module."
+          else
+            add_symbol imports (make_qident (module_name, name, nickname))
+     else
+       err "Declaration is not importable"
   | None ->
      err ("No declaration with the name '"
           ^ (ident_string name)
@@ -46,40 +57,14 @@ let resolve_import (menv: menv) (kind: module_kind) (imports: import_map) (idecl
           ^ (mod_name_string module_name)
           ^ "'")
 
-  match get_module menv module_name with
-  | (Some smodule) ->
-     (match get_declaration (smodule, name) with
-      | (Some decl) ->
-         if is_importable decl then
-           (match get_symbol imports nickname with
-            | (Some _) ->
-               err "Colliding imports"
-            | None ->
-               if (equal_module_name module_name memory_module_name) && (kind = SafeModule) then
-                 err "Cannot import from the Austral.Memory module in a safe module."
-               else
-                 add_symbol imports (make_qident (module_name, name, nickname)))
-         else
-           err "Declaration is not importable"
-      | None ->
-         if has_union_constructor_with_name smodule name then
-           add_symbol imports (make_qident (module_name, name, nickname))
-         else
-           if has_method_with_name smodule name then
-             add_symbol imports (make_qident (module_name, name, nickname))
-           else
-             err ("No declaration with this name: " ^ (ident_string name)))
-  | None ->
-     err ("No module with this name: " ^ (mod_name_string module_name))
-
-let rec resolve' (menv: menv) (kind: module_kind) (imports: import_map) (list: import_decl list): import_map =
+let rec resolve' (env: env) (kind: module_kind) (imports: import_map) (list: import_decl list): import_map =
   match list with
   | (first::rest) ->
-     resolve' menv kind (resolve_import menv kind imports first) rest
+     resolve' env kind (resolve_import env kind imports first) rest
   | [] ->
      imports
 
-let rec add_instances im is =
+let rec add_instances (im: import_map) (is: decl_id list): import_map =
   match is with
   | first::rest ->
      add_instance (add_instances im rest) first
@@ -89,19 +74,20 @@ let rec add_instances im is =
 let module_names (cil: concrete_import_list list): module_name list =
   List.map (fun (ConcreteImportList (mn, _)) -> mn) cil
 
-let module_defined_instances (menv: menv) (mn: module_name): semantic_instance list =
-  match (get_module menv mn) with
-  | Some m ->
-     defined_instances m
+let module_defined_instances (env: env) (mn: module_name): decl_id list =
+  match (get_module_by_name env mn) with
+  | Some mod_rec ->
+     let { id; _ } = mod_rec in
+     module_instances env id
   | None ->
      []
 
-let import_instances (menv: menv) (list: concrete_import_list list): semantic_instance list =
+let import_instances (env: env) (list: concrete_import_list list): decl_id list =
   let mns = module_names list in
-  let instances = List.map (module_defined_instances menv) mns in
+  let instances = List.map (module_defined_instances env) mns in
   List.flatten instances
 
-let resolve (importing_module: module_name) (kind: module_kind) (menv: menv) (list: concrete_import_list list): import_map =
-  let im = resolve' menv kind (empty_map importing_module) (flatten_imports list) in
-  let ins = import_instances menv list in
+let resolve (importing_module: module_name) (kind: module_kind) (env: env) (list: concrete_import_list list): import_map =
+  let im = resolve' env kind (empty_map importing_module) (flatten_imports list) in
+  let ins = import_instances env list in
   add_instances im ins
