0410e64a3cf9b85a5cc5764816bada4c0cb3ebd3
Fernando Borretti <fernando@borretti.me>
2022-03-13 00:27:52 +0000

    Better docstring (thanks ObserverSuns)

--- lib/Util.mli
+++ lib/Util.mli
@@ -66,5 +66,5 @@ val run_command : string -> command_output
     The second argument is the output binary file path. *)
 val compile_cpp_code : string -> string -> command_output
 
-(** Not sure what the standard name for this is. *)
+(** Like `mapAccumL` in Haskell. *)
 val map_with_context : (('c * 'a) -> ('c * 'b)) -> 'c -> 'a list -> ('c * ('b list))
