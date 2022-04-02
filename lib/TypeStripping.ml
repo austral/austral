4eb3b2b1cd18f999bf65b71671cf3204438dfdb8
Fernando Borretti <fernando@borretti.me>
2022-04-02 03:48:42 +0000

    indent

--- lib/TypeStripping.ml
+++ lib/TypeStripping.ml
@@ -17,54 +17,54 @@ type stripped_ty =
 let rec strip_type (ty: ty): stripped_ty =
   match strip_type' ty with
   | Some ty ->
-     ty
+    ty
   | None ->
-     err "strip_type called with a region type as its argument"
+    err "strip_type called with a region type as its argument"
 
 and strip_type' (ty: ty): stripped_ty option =
   match ty with
   | Unit ->
-     Some SUnit
+    Some SUnit
   | Boolean ->
-     Some SBoolean
+    Some SBoolean
   | Integer (s, w) ->
-     Some (SInteger (s, w))
+    Some (SInteger (s, w))
   | SingleFloat ->
-     Some SSingleFloat
+    Some SSingleFloat
   | DoubleFloat ->
-     Some SDoubleFloat
+    Some SDoubleFloat
   | NamedType (n, args, _) ->
-     Some (SNamedType (n, List.filter_map strip_type' args))
+    Some (SNamedType (n, List.filter_map strip_type' args))
   | Array (elem_ty, _) ->
-     (match (strip_type' elem_ty) with
-      | Some elem_ty ->
-         Some (SArray elem_ty)
-      | None ->
-         err "Internal: array instantiated with a region type.")
+    (match (strip_type' elem_ty) with
+     | Some elem_ty ->
+       Some (SArray elem_ty)
+     | None ->
+       err "Internal: array instantiated with a region type.")
   | RegionTy _ ->
-     None
+    None
   | ReadRef (ty, _) ->
-     (match (strip_type' ty) with
-      | Some ty ->
-         Some (SReadRef ty)
-      | None ->
-         err "Internal: read ref instantiated with a region type.")
+    (match (strip_type' ty) with
+     | Some ty ->
+       Some (SReadRef ty)
+     | None ->
+       err "Internal: read ref instantiated with a region type.")
   | WriteRef (ty, _) ->
-     (match (strip_type' ty) with
-      | Some ty ->
-         Some (SWriteRef ty)
-      | None ->
-         err "Internal: write ref instantiated with a region type.")
+    (match (strip_type' ty) with
+     | Some ty ->
+       Some (SWriteRef ty)
+     | None ->
+       err "Internal: write ref instantiated with a region type.")
   | TyVar _ ->
-     (* Why? Because when instantiating a monomorph, we do search and replace of
-        type variables with their substitutions. So if there are variables left
-        over by stripping time, that's an error. Anyways, the search-and-replace
-        step should *also* have signalled an error if a type variable has no
-        replacement. *)
-     err "Variable not replaced."
+    (* Why? Because when instantiating a monomorph, we do search and replace of
+       type variables with their substitutions. So if there are variables left
+       over by stripping time, that's an error. Anyways, the search-and-replace
+       step should *also* have signalled an error if a type variable has no
+       replacement. *)
+    err "Variable not replaced."
   | RawPointer ty ->
-     (match (strip_type' ty) with
-      | Some ty ->
-         Some (SRawPointer ty)
-      | None ->
-         err "Internal: raw pointer type instantiated with a region type.")
+    (match (strip_type' ty) with
+     | Some ty ->
+       Some (SRawPointer ty)
+     | None ->
+       err "Internal: raw pointer type instantiated with a region type.")
