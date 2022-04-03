72d39657d9591f021eaa2fdb4edf878955a4e394
Fernando Borretti <fernando@borretti.me>
2022-04-03 00:21:45 +0000

    Add monomorphize_decl to .mli file

--- lib/Monomorphize.mli
+++ lib/Monomorphize.mli
@@ -12,6 +12,12 @@ val monomorphize_ty : env -> stripped_ty -> (mono_ty * env)
    updated environment. *)
 val monomorphize_expr : env -> texpr -> (mexpr * env)
 
-(** Monomorphize a statement, returning a monomorphic statementand the updated
+(** Monomorphize a statement, returning a monomorphic statement and the updated
    environment. *)
 val monomorphize_stmt : env -> tstmt -> (mstmt * env)
+
+(** Monomorphize a declaration, returning a monomorphic declaration and the
+   updated environment. Return {None} where a declaration has no immediate
+   monomorph (e.g.: generic type definitions, which are monomorphized on
+   demand). *)
+val monomorphize_decl : env -> typed_decl -> (mdecl option * env)
