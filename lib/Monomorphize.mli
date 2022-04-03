02a08acca0d1897be06940ae7fa9aa0afeec0cfb
Fernando Borretti <fernando@borretti.me>
2022-04-03 00:00:29 +0000

    Add documentation comments

--- lib/Monomorphize.mli
+++ lib/Monomorphize.mli
@@ -4,8 +4,14 @@ open MonoType2
 open Tast
 open Mtast
 
+(** Monomorphize a type specifier, returning a monomorphic type and the updated
+   environment. *)
 val monomorphize_ty : env -> stripped_ty -> (mono_ty * env)
 
+(** Monomorphize an expression, returning a monomorphic expression and the
+   updated environment. *)
 val monomorphize_expr : env -> texpr -> (mexpr * env)
 
+(** Monomorphize a statement, returning a monomorphic statementand the updated
+   environment. *)
 val monomorphize_stmt : env -> tstmt -> (mstmt * env)
