cfa75922cb3381b064aa007f62c87b31f5ce7a02
Fernando Borretti <fernando@borretti.me>
2022-05-14 22:21:53 +0000

    Add a TypeParameters module

---
+++ lib/TypeParameters.ml
@@ -0,0 +1,39 @@
+open Identifier
+open Type
+open Error
+
+type typarams = TyParams of type_parameter list
+
+let empty_typarams: typarams = TyParams []
+
+let typaram_name (typaram: type_parameter): identifier =
+  let (TypeParameter (name, _, _)) = typaram in
+  name
+
+let get_typaram (typarams: typarams) (name: identifier): type_parameter option =
+  let (TyParams lst) = typarams in
+  let pred (typaram: type_parameter): bool =
+    equal_identifier name (typaram_name typaram)
+  in
+  List.find_opt pred lst
+
+
+let add_typaram (typarams: typarams) (typaram: type_parameter): typarams =
+  match get_typaram typarams (typaram_name typaram) with
+  | Some _ ->
+     err "Duplicate type parameter."
+  | None ->
+    let (TyParams lst) = typarams in
+    let lst = List.rev lst in
+    let lst = typaram :: lst in
+    let lst = List.rev lst in
+    TyParams lst
+
+let typarams_as_list (typarams: typarams): type_parameter list =
+  let (TyParams lst) = typarams in
+  lst
+
+let typarams_from_list (lst: type_parameter list): typarams =
+  List.fold_left (fun set typaram -> add_typaram set typaram)
+    empty_typarams
+    lst
