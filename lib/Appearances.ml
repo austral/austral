f90c2079b86131f84a125afdefac052824e4db23
Fernando Borretti <fernando@borretti.me>
2022-05-15 20:54:21 +0000

    Implement the table of appearances

---
+++ lib/Appearances.ml
@@ -0,0 +1,43 @@
+open Identifier
+open Ptast
+open Error
+
+type appear_tbl = appear_row list
+
+and appear_row = Row of identifier * loop_context * appear list
+
+and appear = Appear of pos * appear_kind * loop_context
+
+and loop_context = loop_kind list
+
+and loop_kind = CtxWhile | CtxFor
+
+and appear_kind = AppearConsume | AppearPath | AppearBorrow
+
+let empty_appearances: appear_tbl = []
+
+let get_var (tbl: appear_tbl) (name: identifier): appear_row option =
+  List.find_opt (fun (Row (name', _, _)) -> equal_identifier name name') tbl
+
+(** Given a variable's name, pop its row from the table, returning the row and
+    the table without it. *)
+let pop_var (tbl: appear_tbl) (name: identifier): (appear_row * appear_tbl) option =
+  match get_var tbl name with
+  | Some row ->
+     Some (row, List.filter (fun (Row (name', _, _)) -> not (equal_identifier name name')) tbl)
+  | None ->
+     None
+
+let register_var (tbl: appear_tbl) (name: identifier) (ctx: loop_context): appear_tbl =
+  match get_var tbl name with
+  | Some _ ->
+     err "Duplicate entry in the table of appearances"
+  | None ->
+     Row (name, ctx, []) :: tbl
+
+let register_appear (tbl: appear_tbl) (name: identifier) (pos: pos) (kind: appear_kind) (ctx: loop_context): appear_tbl =
+  match pop_var tbl name with
+  | Some (Row (_, init_ctx, appearances), tbl) ->
+     (Row (name, init_ctx, Appear (pos, kind, ctx) :: appearances)) :: tbl
+  | None ->
+     err "No variable with that name."
---
+++ lib/Appearances.mli
@@ -0,0 +1,43 @@
+(** The table of appearances is used by the lifetime analysis pass. *)
+open Identifier
+open Ptast
+
+(** The table of appearances maps linear variables to the list of places where
+    they appear, and the loop context where the variable was defined. *)
+type appear_tbl
+
+(** A loop is either a while loop or a for loop. *)
+type loop_kind = CtxWhile | CtxFor
+
+(** A loop context is best explained with an example. Consider:
+
+    while ... do
+        let x: T := Make_T();
+        for i from 0 to n do
+            while ... do
+                consume(x);
+            end while;
+        end for;
+    end while;
+
+    Here, the loop context where `x` is defined is `[CtxWhile]`, and the loop
+    context where `x` is consumed is `[CtxWhile, CtxFor, CtxWhile]`. This is
+    used to verify that a linear variable does not get consumed inside a loop.
+ *)
+type loop_context = loop_kind list
+
+(** A linear variable can appear in three ways: being consumed, being at the
+    head of a path, or being borrowed. *)
+type appear_kind = AppearConsume | AppearPath | AppearBorrow
+
+(** The empty table. *)
+val empty_appearances : appear_tbl
+
+(** Given a variable's name and the loop context where it is defined, add a row
+    to the table of appearances. Errors if a variable with that name already
+    exists. *)
+val register_var : appear_tbl -> identifier -> loop_context -> appear_tbl
+
+(** Given a variable's name, register an appearance with a position, type, and
+    the loop context where the variable appeared. *)
+val register_appear : appear_tbl -> identifier -> pos -> appear_kind -> loop_context -> appear_tbl
