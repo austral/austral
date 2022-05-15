cd7d56812534cceabb4b82a9c4061aa91f0506a9
Fernando Borretti <fernando@borretti.me>
2022-05-15 00:47:31 +0000

    Add borrow expressions to CST

--- lib/Cst.ml
+++ lib/Cst.ml
@@ -84,6 +84,7 @@ and cexpr =
   | CDeref of span * cexpr
   | CTypecast of span * cexpr * typespec
   | CSizeOf of span * typespec
+  | CBorrowExpr of span * borrowing_mode * identifier
 
 and cstmt =
   | CSkip of span
