4cbc36fc2c244bef4b99649a6e7e243b43e24364
Fernando Borretti <fernando@borretti.me>
2022-05-19 21:19:16 +0000

    Add a function to extract variable appearances from an expression

--- lib/LifetimeAnalysis.ml
+++ lib/LifetimeAnalysis.ml
@@ -1,6 +1,10 @@
+open Identifier
 open Type
 open TypeSystem
 open Appearances
+open Tast
+open Ptast
+open Error
 
 (* True when a universe is Linear or Type. *)
 let universe_linear_ish = function
@@ -26,3 +30,127 @@ let appearances_from_params (params: value_parameter list): appear_tbl =
       tbl
   in
   List.fold_left folder tbl params
+
+let rec extract_variables (tbl: appear_tbl) (expr: texpr): (identifier * appear_kind) list =
+  match expr with
+  | TNilConstant ->
+     []
+  | TBoolConstant _ ->
+     []
+  | TIntConstant _ ->
+     []
+  | TFloatConstant _ ->
+     []
+  | TStringConstant _ ->
+     []
+  | TConstVar _ ->
+     (* Constants can't be linear so we don't need to extract them. *)
+     []
+  | TParamVar (name, _) ->
+     (* If the name is mentioned in the table, extract it. *)
+     if tbl_has_name tbl name then
+       [(name, AppearConsume)]
+     else
+       []
+  | TLocalVar (name, _) ->
+     (* If the name is mentioned in the table, extract it. *)
+     if tbl_has_name tbl name then
+       [(name, AppearConsume)]
+     else
+       []
+  | TArithmetic (_, lhs, rhs) ->
+     let lhs' = extract_variables tbl lhs in
+     let rhs' = extract_variables tbl rhs in
+     lhs' @ rhs'
+  | TFuncall (_, _, args, _, _) ->
+     let args' = List.map (extract_variables tbl) args in
+     List.concat args'
+  | TMethodCall (_, _, _, args, _, _) ->
+     let args' = List.map (extract_variables tbl) args in
+     List.concat args'
+  | TCast (expr, _) ->
+     extract_variables tbl expr
+  | TComparison (_, lhs, rhs) ->
+     let lhs' = extract_variables tbl lhs in
+     let rhs' = extract_variables tbl rhs in
+     lhs' @ rhs'
+  | TConjunction (lhs, rhs) ->
+     let lhs' = extract_variables tbl lhs in
+     let rhs' = extract_variables tbl rhs in
+     lhs' @ rhs'
+  | TDisjunction (lhs, rhs) ->
+     let lhs' = extract_variables tbl lhs in
+     let rhs' = extract_variables tbl rhs in
+     lhs' @ rhs'
+  | TNegation expr ->
+     extract_variables tbl expr
+  | TIfExpression (c, t, f) ->
+     let c' = extract_variables tbl c
+     and t' = extract_variables tbl t
+     and f' = extract_variables tbl f
+     in
+     c' @ t' @ f'
+  | TRecordConstructor (_, args) ->
+      let args' = List.map (fun (_, e) -> extract_variables tbl e) args in
+      List.concat args'
+  | TUnionConstructor (_, _, args) ->
+      let args' = List.map (fun (_, e) -> extract_variables tbl e) args in
+      List.concat args'
+  | TTypeAliasConstructor (_, e) ->
+     extract_variables tbl e
+  | TPath { head; elems; _ } ->
+     let head' =
+       (match head with
+        (* If the head is a variable, extract is with AppearPath. *)
+        | TParamVar (name, _) ->
+           if tbl_has_name tbl name then
+             [(name, AppearPath)]
+           else
+             []
+        | TLocalVar (name, _) ->
+           if tbl_has_name tbl name then
+             [(name, AppearPath)]
+           else
+             []
+        | _ ->
+           (* Otherwise, just extract the variables inside. *)
+           extract_variables tbl head)
+     and elems' = List.map (extract_path_variables tbl) elems
+     in
+     head' @ (List.concat elems')
+  | TEmbed (_, _, args) ->
+     let args' = List.map (extract_variables tbl) args in
+     List.concat args'
+  | TDeref e ->
+     extract_variables tbl e
+  | TSizeOf _ ->
+     []
+  | TBorrowExpr (_, name, _, _) ->
+     if tbl_has_name tbl name then
+       [(name, AppearBorrow)]
+     else
+       []
+
+and extract_path_variables (tbl: appear_tbl) (elem: typed_path_elem): (identifier * appear_kind) list =
+  match elem with
+  | TSlotAccessor _ ->
+     []
+  | TPointerSlotAccessor _ ->
+     []
+  | TArrayIndex (e, _) ->
+     extract_variables tbl e
+
+let rec record_appearances (tbl: appear_tbl) (stmt: pstmt): appear_tbl =
+  record_appearances' tbl [] stmt
+
+and record_appearances' (tbl: appear_tbl) (loop_ctx: loop_context) (stmt: pstmt): appear_tbl =
+  match stmt with
+  | PSkip _ ->
+     tbl
+  | PLet (pos, _, name, _ ,_ , body) ->
+     let tbl = register_var tbl name pos loop_ctx in
+     record_appearances' tbl loop_ctx body
+  | PLetBorrow { body; _ } ->
+     record_appearances' tbl loop_ctx body
+  | _ ->
+     err "Not implemented"
--- lib/LifetimeAnalysis.mli
+++ lib/LifetimeAnalysis.mli
@@ -1,7 +1,18 @@
 (** This module implements Austral's lifetime analysis pass. *)
+open Identifier
 open Type
 open Appearances
+open Tast
+open Ptast
 
 (** Given a list of value parameters, return a table of appearances where linear
     parameters are stored in the table as variables. *)
 val appearances_from_params : value_parameter list -> appear_tbl
+
+(** Given an expression, extract all the variables in the expression that are
+    mentioned in the table of appearances, along with the kind of appearance
+    they have. *)
+val extract_variables : appear_tbl -> texpr -> (identifier * appear_kind) list
+
+(** Given a statement, do a depth-first pass and record variable appearances in the table. *)
+val record_appearances : appear_tbl -> pstmt -> appear_tbl
