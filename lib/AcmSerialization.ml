cfa3822f2d851f0b49d980a36bf9f52d24611aab
Fernando Borretti <fernando@borretti.me>
2022-06-12 23:53:46 +0000

    Add WIP acm serialization/deserialization module

---
+++ lib/AcmSerialization.ml
@@ -0,0 +1,19 @@
+open Sexplib
+open CompiledModule
+open Error
+
+type sexp = Sexp.t
+
+let fail (msg: string) =
+  err ("ACM file parsing failed: " ^ msg)
+
+let ser_acm_type_vis (vis: acm_type_vis): sexp =
+  match vis with
+  | AcmTypeVisPublic -> Atom "ACM_TYPE_VIS_PUBLIC"
+  | AcmTypeVisOpaque -> Atom "ACM_TYPE_VIS_OPAQUE"
+
+let par_acm_type_vis (sexp: sexp): acm_type_vis =
+  match sexp with
+  | Atom "ACM_TYPE_VIS_PUBLIC" -> AcmTypeVisPublic
+  | Atom "ACM_TYPE_VIS_OPAQUE" -> AcmTypeVisOpaque
+  | _ -> fail "bad acm_type_vis"
---
+++ lib/AcmSerialization.mli
@@ -0,0 +1,8 @@
+(** Serializing and deserializing ACM files. *)
+open Sexplib
+open CompiledModule
+
+type sexp = Sexp.t
+
+val ser_acm_type_vis : acm_type_vis -> sexp
+val par_acm_type_vis : sexp -> acm_type_vis
