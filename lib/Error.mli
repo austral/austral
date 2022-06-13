ed328be857965b88786ced28eb2b73d0c5658c9d
Fernando Borretti <fernando@borretti.me>
2022-06-13 22:21:05 +0000

    Overhaul the Error module

--- lib/Error.mli
+++ lib/Error.mli
@@ -1,18 +1,40 @@
+(** Error reporting infrastructure. *)
+open Identifier
 open Span
+open SourceContext
+open ErrorText
 
-type error
-type error_data
+(** An Austral compiler error contains: the name of the module where the error
+    occurred (if available), the error title, the error description, the span of code where the
+    error occured (if available), the source context associated to that span (if
+    available).
 
-val error_filename : error -> string option
-val error_text : error_data -> string
-val error_title : error_data -> string
+    The span and source context rarely have to be passed in explicitly: they are
+    added where the error is throw in the context of
+    {!adorn_error_with_span}. *)
+type austral_error = AustralError of {
+      module_name: module_name option;
+      title: string;
+      text: err_text;
+      span: span option;
+      source_ctx: source_ctx option;
+    }
 
-val render_error : error -> string option -> string
+(** The exception that carries Austral errors. *)
+exception Austral_error of austral_error
 
-exception Austral_error of error
+(** Raise an Austral error given the title and text. *)
+val austral_raise : string -> err_text -> 'a
 
-val err : string -> 'a
-val raise_parse_error : span -> 'a
-val raise_type_mismatch_error : string -> string -> 'a
+(** Add a source context to an error if it doesn't have one. *)
+val add_source_ctx : austral_error -> source_ctx -> austral_error
 
+(** Run the callback, and if it throws an error that doesn't have a span, put
+    the given span in the error and rethrow it. *)
 val adorn_error_with_span : span -> (unit -> 'a) -> 'a
+
+(** Render an error to plain text. *)
+val render_error_to_plain : austral_error -> string
+
+(** Utility: raise a generic error. *)
+val err : string -> 'a
