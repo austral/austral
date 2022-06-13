0524aa1e67bc2da68c32d5119d11db0f3d554adc
Fernando Borretti <fernando@borretti.me>
2022-06-13 22:20:50 +0000

    Add a utility module to represent and render error message text

---
+++ lib/ErrorText.mli
@@ -0,0 +1,17 @@
+(** This module provides a representation-independent way of writing the text in
+    error messages. Rather than concatenating strings, we use a more structured
+    representation, which can then be rendered to both plain text and HTML. *)
+
+(** The contents of an error message. *)
+type err_text = text_elem list
+
+(** An individual error text element. *)
+and text_elem =
+  | Text of string
+  (** Human-readable prose. *)
+  | Code of string
+  (** Austral text, like the name of an identifier or a type. *)
+  | Break
+  (** A paragraph break. *)
+
+val error_text_to_plain : err_text -> string
