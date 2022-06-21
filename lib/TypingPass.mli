d79e51c8d2cdf66fc24ea28c2b43564a24237ce0
Fernando Borretti <fernando@borretti.me>
2022-06-21 20:55:36 +0000

    Export cast_arguments

--- lib/TypingPass.mli
+++ lib/TypingPass.mli
@@ -4,6 +4,8 @@ open Env
 open Ast
 open Linked
 open Tast
+open TypeBindings
+open Type
 
 type stmt_ctx
 
@@ -12,3 +14,5 @@ val augment_stmt: stmt_ctx -> astmt -> tstmt
 val augment_decl : module_name -> module_kind -> env -> linked_definition -> typed_decl
 
 val augment_module : env -> linked_module -> typed_module
+
+val cast_arguments : type_bindings -> value_parameter list -> texpr list -> texpr list
