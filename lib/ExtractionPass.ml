31bb5c79e15a5060da03e3977fc3897898f2bb28
Fernando Borretti <fernando@borretti.me>
2022-06-24 19:29:29 +0000

    Add a check that free records don't contain linear types

--- lib/ExtractionPass.ml
+++ lib/ExtractionPass.ml
@@ -19,6 +19,21 @@ open TypeClasses
 open Util
 open Error
 
+let check_slots_are_free (slots: typed_slot list): unit =
+  let check_slot_is_free (slot: typed_slot): unit =
+    let (TypedSlot (_, ty)) = slot in
+    match type_universe ty with
+    | FreeUniverse ->
+       ()
+    | RegionUniverse ->
+       ()
+    | LinearUniverse ->
+       err "Record was declared to belong to the Free universe, but it contains a type that beloings to the Linear universe."
+    | TypeUniverse ->
+       err "Record was declared to belong to the Free universe, but it contains a type that beloings to the Type universe."
+  in
+  List.iter check_slot_is_free slots
+
 let rec extract_type_signatures (CombinedModule { decls; _ }): type_signature list =
   List.filter_map extract_type_signatures' decls
 
@@ -129,6 +144,13 @@ and extract_definition (env: env) (mod_id: mod_id) (local_types: type_signature 
      (env, decl)
   | CRecord (vis, name, typarams, universe, slots, docstring) ->
      let slots = List.map (parse_slot typarams) slots in
+     (* If the universe is `Free`, check that there are no slots with types in the `Type` or `Linear` universes. *)
+     let _ =
+       if universe = FreeUniverse then
+         check_slots_are_free slots
+       else
+         ()
+     in
      let (env, decl_id) = add_record env { mod_id; vis; name; docstring; typarams; universe; slots } in
      let decl = LRecord (decl_id, vis, name, typarams, universe, slots, docstring) in
      (env, decl)
