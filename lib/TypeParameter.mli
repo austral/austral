e5f2264870eea023b738bf9d9014ce16451f8391
Fernando Borretti <fernando@borretti.me>
2022-08-19 23:35:30 +0000

    Add docstrings

--- lib/TypeParameter.mli
+++ lib/TypeParameter.mli
@@ -6,14 +6,21 @@ open Type
 type type_parameter
 [@@deriving (show, sexp)]
 
+(** Construct a type parameter. *)
 val make_typaram : identifier * universe * qident * sident list -> type_parameter
 
+(** The type parameter's name. *)
 val typaram_name : type_parameter -> identifier
 
+(** The type parameter's universe. *)
 val typaram_universe : type_parameter -> universe
 
+(** The name of the declaration this type parameter belongs to. *)
 val typaram_source : type_parameter -> qident
 
+(** The type parameter's constraints, i.e., a list of the names of typeclasses
+    arguments to this type must implement. *)
 val typaram_constraints : type_parameter -> sident list
 
+(** Transform a type parameter into an equivalent type variable. *)
 val typaram_to_tyvar : type_parameter -> type_var
