8f0699f8bb53347af80480832798dfa4bc98929c
Fernando Borretti <fernando@borretti.me>
2023-01-08 17:46:26 +1100

    Pass -y to opam install

--- install-ocaml-deps.sh
+++ install-ocaml-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Install a specific version of ppxlib.
-opam install ppxlib.0.25.0
+opam install -y ppxlib.0.25.0
 # Install dependencies.
-opam install dune ppx_deriving ounit2 menhir sexplib ppx_sexp_conv
+opam install -y dune ppx_deriving ounit2 menhir sexplib ppx_sexp_conv
