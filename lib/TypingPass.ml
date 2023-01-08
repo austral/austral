b9bfb67fa1d8f725d63b5cea2dc8cbdd25a03e8e
Fernando Borretti <fernando@borretti.me>
2023-01-08 11:43:42 +1100

    End with a period

--- lib/TypingPass.ml
+++ lib/TypingPass.ml
@@ -115,7 +115,8 @@ let rec augment_expr (module_name: module_name) (env: env) (rm: region_map) (typ
                Text "Both operands to an arithmetic expression must be compatible types. The LHS has type";
                Code (type_string lhs_ty);
                Text "but the RHS has type";
-               Code (type_string rhs_ty)
+               Code (type_string rhs_ty);
+               Text "."
              ]
          in
          (* Are the types the same? *)
