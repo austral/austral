c80330ed74db6dad99fd98f96af9244bcf63709e
Fernando Borretti <fernando@borretti.me>
2023-03-11 14:07:33 +1100

    Add docstrings

--- lib/LexEnv.mli
+++ lib/LexEnv.mli
@@ -8,19 +8,27 @@
 open Identifier
 open Type
 
+(** A lexical environment. *)
 type lexenv
 
+(** Where an identifier comes from: a constant, a function parameter, or a local
+    variable. *)
 type var_source =
   | VarConstant
   | VarParam
   | VarLocal
 
+(** The empty lexenv. *)
 val empty_lexenv : lexenv
 
+(** Add a variable to the lexenv given its name, type, and source. *)
 val push_var : lexenv -> identifier -> ty -> var_source -> lexenv
 
+(** Add a list of variables. *)
 val push_vars : lexenv -> (identifier * ty * var_source) list -> lexenv
 
+(** Remove the last variable added. *)
 val pop_var : lexenv -> lexenv
 
+(** Retrieve a variable by name. *)
 val get_var : lexenv -> identifier -> (ty * var_source) option
