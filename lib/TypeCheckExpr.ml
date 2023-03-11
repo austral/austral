2d324072621e860e0976920512423b41b0641932
Fernando Borretti <fernando@borretti.me>
2023-03-11 12:27:20 +1100

    Basic dispatch

--- lib/TypeCheckExpr.ml
+++ lib/TypeCheckExpr.ml
@@ -66,7 +66,12 @@ let parse_typespec (env: env) (rm: region_map) (typarams: typarams) (ty: qtypesp
 (* Interface *)
 
 let rec augment_expr (ctx: expr_ctx) (asserted_ty: ty option) (expr: aexpr): texpr =
+  let _ = (ctx, asserted_ty, expr) in
   (* `aug` is a shortmhand for `augment_expr` with `None` as the asserted type. *)
   let aug = augment_expr ctx None in
-  let _ = (ctx, asserted_ty, expr) in
-  internal_err "Not implemented yet"
+  (* Dispatch on the AST *)
+  match expr with
+  | NilConstant ->
+     TNilConstant
+  | _ ->
+     internal_err "Not implemented yet"
