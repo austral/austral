d1354e9f577ca6244655864f03e2608d4db94d75
Fernando Borretti <fernando@borretti.me>
2023-04-01 09:58:18 +1100

    Add type information to the linearity checker

--- lib/LinearityCheck.mli
+++ lib/LinearityCheck.mli
@@ -29,10 +29,10 @@ type state_tbl
 val empty_tbl : state_tbl
 
 (** Find an entry in the state table. *)
-val get_entry : state_tbl -> identifier -> (loop_depth * var_state) option
+val get_entry : state_tbl -> identifier -> (ty * loop_depth * var_state) option
 
 (** Add a new entry to the state table. Throws an error if an entry with that name already exists. All new variables start as unconsumed. *)
-val add_entry : state_tbl -> identifier -> loop_depth -> state_tbl
+val add_entry : state_tbl -> identifier -> ty -> loop_depth -> state_tbl
 
 (** Update the state of a variable in the table. Throws an error if there is no entry with this name. *)
 val update_tbl : state_tbl -> identifier -> var_state -> state_tbl
@@ -50,7 +50,7 @@ val mark_pending : state_tbl -> identifier -> state_tbl
 val remove_pending : state_tbl -> identifier -> state_tbl
 
 (** Return all entries as a list. *)
-val tbl_to_list : state_tbl -> (identifier * loop_depth * var_state) list
+val tbl_to_list : state_tbl -> (identifier * ty * loop_depth * var_state) list
 
 (** Get the list of pending variables. *)
 val get_pending : state_tbl -> identifier list
