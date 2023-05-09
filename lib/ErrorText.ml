d47771d1327c898c51cf983e4989285d3ce1da12
Fernando Borretti <fernando@borretti.me>
2023-05-09 16:25:52 +1000

    trim whitespace

--- lib/ErrorText.ml
+++ lib/ErrorText.ml
@@ -27,7 +27,7 @@ and text_elem_to_plain (elem: text_elem): string =
      "`" ^ (type_string ty) ^ "`"
   | Break ->
      "\n\n"
- 
+
 let rec error_text_to_html (txt: err_text): string =
     String.concat "" (List.map text_elem_to_html txt)
 
