f1c940e9efce8cac0ac87248cfa84924c0a75c91
Fernando Borretti <fernando@borretti.me>
2023-06-03 14:10:46 +1000

    Add AstDB module

--- lib/Stages.ml
+++ lib/Stages.ml
@@ -97,3 +97,83 @@ module Ast = struct
   and lvalue =
     LValue of identifier * path_elem list
 end
+
+
+(** The AST, but anonymous borrows are desugard. *)
+module AstDB = struct
+  open Identifier
+  open Common
+  open Span
+  open Escape
+
+  type qtypespec = Ast.qtypespec
+
+  type qbinding = Ast.qbinding
+
+  type astmt =
+    | ASkip of span
+    | ALet of span * mutability * identifier * qtypespec * aexpr * astmt
+    | ADestructure of span * mutability * qbinding list * aexpr * astmt
+    | AAssign of span * lvalue * aexpr
+    | AIf of span * aexpr * astmt * astmt
+    | AWhen of span * aexpr * astmt
+    | ACase of span * aexpr * abstract_when list
+    | AWhile of span * aexpr * astmt
+    | AFor of {
+        span: span;
+        name: identifier;
+        initial: aexpr;
+        final: aexpr;
+        body: astmt
+      }
+    | ABorrow of {
+        span: span;
+        original: identifier;
+        rename: identifier;
+        region: identifier;
+        body: astmt;
+        mode: borrowing_mode
+      }
+    | ABlock of span * astmt * astmt
+    | ADiscarding of span * aexpr
+    | AReturn of span * aexpr
+
+  and aexpr =
+    | NilConstant
+    | BoolConstant of bool
+    | IntConstant of string
+    | FloatConstant of string
+    | StringConstant of escaped_string
+    | Variable of qident
+    | FunctionCall of qident * abstract_arglist
+    | ArithmeticExpression of arithmetic_operator * aexpr * aexpr
+    | Comparison of comparison_operator * aexpr * aexpr
+    | Conjunction of aexpr * aexpr
+    | Disjunction of aexpr * aexpr
+    | Negation of aexpr
+    | IfExpression of aexpr * aexpr * aexpr
+    | Path of aexpr * path_elem list
+    | RefPath of aexpr * ref_path_elem list
+    | Embed of qtypespec * string * aexpr list
+    | Deref of aexpr
+    | Typecast of aexpr * qtypespec
+    | SizeOf of qtypespec
+
+  and abstract_when =
+    | AbstractWhen of identifier * qbinding list * astmt
+
+  and abstract_arglist =
+    | Positional of aexpr list
+    | Named of (identifier * aexpr) list
+
+  and path_elem =
+    | SlotAccessor of identifier
+    | PointerSlotAccessor of identifier
+    | ArrayIndex of aexpr
+
+  and ref_path_elem =
+    | RefSlotAccessor of identifier
+
+  and lvalue =
+    LValue of identifier * path_elem list
+end
