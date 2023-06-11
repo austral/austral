dc53ba059a11f4b8da204e8647c26cd68f87998c
Fernando Borretti <fernando@borretti.me>
2023-06-11 11:26:25 +1000

    Add Temporary module

---
+++ lib/Temporary.mli
@@ -0,0 +1,12 @@
+(*
+   Part of the Austral project, under the Apache License v2.0 with LLVM Exceptions.
+   See LICENSE file for details.
+
+   SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *)
+
+type tmp
+
+val fresh_tmp : unit -> tmp
+
+val tmp_to_string : tmp -> string
