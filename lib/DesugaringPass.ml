9f6df180b8dbd33e1303c06b46616e574c7dbbbc
Fernando Borretti <fernando@borretti.me>
2023-06-11 14:51:16 +1000

    Fix DesugaringPass

--- lib/DesugaringPass.ml
+++ lib/DesugaringPass.ml
@@ -10,10 +10,10 @@ module C = Combined
 module SC = SmallCombined
 
 let te (expr: Ast.aexpr): AstDB.aexpr =
-  DesugarBorrows.transform_expr (LiftControlPass.transform expr)
+  DesugarBorrows.transform_expr (DesugarPaths.transform_expr (LiftControlPass.transform expr))
 
 let ts (stmt: Ast.astmt): AstDB.astmt =
-  DesugarBorrows.transform_stmt (LiftControlPass.lift stmt)
+  DesugarBorrows.transform_stmt (DesugarPaths.transform_stmt (LiftControlPass.lift stmt))
 
 let copy_combined_method_def (C.CMethodDef (id, tp, qp, qt, ds, ast)) =
   SC.CMethodDef (id, tp, qp, qt, ds, ts ast)
