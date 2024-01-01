6cbb45f58734b15db42fcbd9c12ae02cabb09d88
Jasper Reef <51498987+Blugatroff@users.noreply.github.com>
2024-01-01 02:15:27 +0100

    add Example.Greet (#578)
    fixes #545
    
    I have had to modify the run-examples.sh to use the stdlib and to
    provide some input to stdin.
    
    This example doesn't actually work correctly yet because
    Standard.IO.Terminal.readLine doesn't work correctly, see #577.

--- run-examples.sh
+++ run-examples.sh
@@ -9,8 +9,22 @@ set -euxo pipefail
 dune build
 
 function compile() {
-    ./austral compile $1/$2.aui,$1/$2.aum --entrypoint=Example.$2:main --output=testbin
-    ./testbin > actual.txt
+    ./austral compile \
+        ./standard/src/Buffer.aui,./standard/src/Buffer.aum \
+        ./standard/src/String.aui,./standard/src/String.aum \
+        ./standard/src/StringBuilder.aui,./standard/src/StringBuilder.aum \
+        ./standard/src/IO/IO.aui,./standard/src/IO/IO.aum \
+        ./standard/src/IO/Terminal.aui,./standard/src/IO/Terminal.aum \
+        $1/$2.aui,$1/$2.aum \
+        --entrypoint=Example.$2:main --output=testbin
+
+    if [ $# -eq 4 ] 
+    then
+        echo -n -e $4 | ./testbin > actual.txt
+    else
+        ./testbin > actual.txt
+    fi
+
     echo -n -e "$3" > expected.txt
     diff actual.txt expected.txt
     rm testbin
@@ -29,3 +43,4 @@ compile examples/memory Memory ""
 compile examples/named-argument NamedArgument ""
 compile examples/record Record ""
 compile examples/union Union ""
+compile examples/greet Greet "Hello, Santa!\n" "Santa"
